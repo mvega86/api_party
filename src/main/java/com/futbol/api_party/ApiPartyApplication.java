6e807fc4e85984f4da7b214ff5bc497a84f2393c
Magdiel <mvg140386@gmail.com>
2025-03-08 17:47:06 +0100

    Se agrega el flyway para que actualice y haga un versionado de las migraciones de las base de datos.

--- src/main/java/com/futbol/api_party/ApiPartyApplication.java
+++ src/main/java/com/futbol/api_party/ApiPartyApplication.java
@@ -1,13 +1,25 @@
 package com.futbol.api_party;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ApiPartyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiPartyApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(@Autowired DataSource dataSource) {
+		return args -> {
+			System.out.println("Conexión a la base de datos establecida: " + dataSource.getConnection().getMetaData().getURL());
+		};
 	}
 
 }
