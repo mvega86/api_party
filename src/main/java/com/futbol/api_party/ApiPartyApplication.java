c56053e07ff9b76545b830616a06a04383511392
Magdiel <mvg140386@gmail.com>
2025-03-08 15:47:34 +0100

    Proyecto inicial

---
+++ src/main/java/com/futbol/api_party/ApiPartyApplication.java
@@ -0,0 +1,13 @@
+package com.futbol.api_party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPartyApplication.class, args);
+	}
+
+}
